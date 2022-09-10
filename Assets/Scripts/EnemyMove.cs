fba0e10a640b0659db538f16cf254c2920a5a097
soccer4kzm8 <soccer4.kzm8@gmail.com>
2022-09-10 16:15:35 +0900

    add Sword and Animation

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
 using UnityEngine.AI;
-using UniRx;
-using UniRx.Triggers;
 
 public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
-    [SerializeField] private GameObject _attackRange;
     [SerializeField] private float _moveSpeed = 3f;
     private NavMeshAgent _navMeshAgent = null; 
 
@@ -14,20 +11,13 @@ public class EnemyMove : MonoBehaviour
     {
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
         _navMeshAgent.speed = _moveSpeed;
-
-        _attackRange.OnTriggerEnterAsObservable()
-            .Where(x => x.gameObject.name == _player.name)
-            .Subscribe(_ => Debug.LogError("îÕàÕÇ…ì¸Ç¡ÇΩ"));
-        _attackRange.OnTriggerExitAsObservable()
-            .Where(x => x.gameObject.name == _player.name)
-            .Subscribe(_ => Debug.LogError("îÕàÕÇ©ÇÁèoÇΩ"));
     }
 
     void Update()
     {
         if(_navMeshAgent.pathStatus != NavMeshPathStatus.PathInvalid)
         {
-            _navMeshAgent.SetDestination(_player.transform.position);
+            //_navMeshAgent.SetDestination(_player.transform.position);
         }
     }
 }
