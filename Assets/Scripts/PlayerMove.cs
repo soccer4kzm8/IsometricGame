a3e1f95438bcf38e56b8cbd2477c0e95d3614de3
kazumatakata0808 <takataka@capa.co.jp>
2022-09-23 20:41:58 +0900

    プレイヤーの動きを修正

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,27 +3,43 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 4f;
+    [SerializeField] private float _turnSpeed = 90f;
     private IInputEventProvider _inputEventProvider;
-    private Vector3 test;
+    private Rigidbody _rb;
 
-    private Vector3 forward, right;
-    void Start()
+    private void Start()
     {
         _inputEventProvider = GetComponent<IInputEventProvider>();
-        forward = Camera.main.transform.forward;
-        forward.y = 0;
-        forward = Vector3.Normalize(forward);
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+        _rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    private void Update()
     {
-        Vector3 rightMovement = right * _moveSpeed * Time.deltaTime * _inputEventProvider.MoveDirection.Value.x;
-        Vector3 upMovement = forward * _moveSpeed * Time.deltaTime * _inputEventProvider.MoveDirection.Value.z;
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
-		this.transform.forward = heading;
-
-		this.transform.position += rightMovement;
-        this.transform.position += upMovement;
+        Look();
     }
+
+	private void FixedUpdate()
+	{
+        Move();
+	}
+
+	private void Look()
+	{
+        if (_inputEventProvider.MoveDirection.Value == Vector3.zero)
+            return;
+
+        var rot = Quaternion.LookRotation(Helpers.ToIso(_inputEventProvider.MoveDirection.Value), Vector3.up);
+        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rot, _turnSpeed * Time.deltaTime);
+    }
+
+	private void Move()
+	{
+        _rb.MovePosition(this.transform.position + this.transform.forward * _inputEventProvider.MoveDirection.Value.normalized.magnitude * _moveSpeed * Time.deltaTime);
+    }
+}
+
+public static class Helpers
+{
+    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+    public static Vector3 ToIso(Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
 }
