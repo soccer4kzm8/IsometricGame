3bf40852e82332ef7c4a703d79de980376ef4959
kazumatakata0808 <takataka@capa.co.jp>
2022-10-07 00:52:51 +0900

    敵が剣に当たったら敵がその場に留まる処理を追加

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,9 +1,12 @@
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _sword;
     [SerializeField] private float _moveSpeed = 3f;
     private NavMeshAgent _navMeshAgent = null; 
 
@@ -11,6 +14,12 @@ public class EnemyMove : MonoBehaviour
     {
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
         _navMeshAgent.speed = _moveSpeed;
+        this.OnTriggerEnterAsObservable()
+            .Where(x => x.gameObject.name == _sword.name)
+            .Subscribe(x => _navMeshAgent.isStopped = true);
+        this.OnTriggerExitAsObservable()
+            .Where(x => x.gameObject.name == _sword.name)
+            .Subscribe(x => _navMeshAgent.isStopped = false);
     }
 
     void FixedUpdate()
