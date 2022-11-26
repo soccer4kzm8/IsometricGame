b5d191451b6f4e940b57354cca30aa4b59b28e09
soccer4kzm8 <soccer4.kzm8@gmail.com>
2022-11-26 13:22:21 +0900

    プッシュできるかの確認

--- Assets/Scripts/HPPresenter.cs
+++ Assets/Scripts/HPPresenter.cs
@@ -5,8 +5,12 @@ public class HPPresenter : MonoBehaviour
 {
     private HPModel _hPModel;
     private HPView _hpView;
+    private IGetHitEventProvider _getHitEventProvider;
+
     void Start()
     {
+        _getHitEventProvider = this.GetComponent<IGetHitEventProvider>();
+
         //_hPModel = new HPModel(100, 100);
         //_hpView = this.GetComponent<HPView>();
         //_hPModel.hpRP.Subscribe(damage => 
