d17d7b92d16b5ee884a9211f9ab286827153c3d7
soccer4kzm8 <soccer4.kzm8@gmail.com>
2022-12-04 01:19:25 +0900

    quitボタンを押すとStartSceneに戻るように変更

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class StartSceneView : MonoBehaviour
+public class GameSceneManager : MonoBehaviour
 {
     [SerializeField] private Button _startButton;
 
     private void Start()
     {
-        _startButton.onClick.AddListener(() => SceneManager.LoadScene("GameScene"));
+        _startButton.onClick.AddListener(() => SceneManager.LoadScene("StartScene"));
     }
 }
