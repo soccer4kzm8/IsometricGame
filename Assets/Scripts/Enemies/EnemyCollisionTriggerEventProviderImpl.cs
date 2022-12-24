47bc7691864ba3d4402d73602fd34ebc56848f80
soccer4kzm8 <soccer4.kzm8@gmail.com>
2022-12-24 14:25:53 +0900

    敵のEventProviderImplを追加

---
+++ Assets/Scripts/Enemies/EnemyCollisionTriggerEventProviderImpl.cs
@@ -0,0 +1,77 @@
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+public class EnemyCollisionTriggerEventProviderImpl : MonoBehaviour, IGetHitEventProvider, IInSightEventProvider
+{
+    #region SerializeField
+    /// <summary>
+	/// 攻撃範囲
+	/// </summary>
+	[SerializeField] private GameObject _attackRange;
+
+	/// <summary>
+	/// 相手の攻撃パーツ名
+	/// </summary>
+	[SerializeField] private string _opponentAttackPartsName;
+
+	/// <summary>
+	/// 視界角度
+	/// </summary>
+	[SerializeField] private float _sightAngle;
+
+	/// <summary>
+	/// 攻撃を受けるパーツ
+	/// </summary>
+	[SerializeField] private GameObject _damagedParts;
+
+	/// <summary>
+	/// 相手のアニメーター
+	/// </summary>
+	[SerializeField] private Animator _opponentAnimator;
+	#endregion SerializeField
+	private readonly ReactiveProperty<bool> _inSight = new ReactiveProperty<bool>();
+    private readonly ReactiveProperty<bool> _getHit = new ReactiveProperty<bool>();
+
+    public IReadOnlyReactiveProperty<bool> InSight => _inSight;
+    public IReadOnlyReactiveProperty<bool> GetHit => _getHit;
+
+    void Start()
+    {
+		_attackRange.OnTriggerStayAsObservable()
+					.Where(x => x.gameObject.name == _opponentAttackPartsName)
+					.Where(x => InSightCheck(x, _sightAngle) == true)
+					.Subscribe(_ => _inSight.Value = true);
+		_attackRange.OnTriggerStayAsObservable()
+					.Where(x => x.gameObject.name == _opponentAttackPartsName)
+					.Where(x => InSightCheck(x, _sightAngle) == false)
+					.Subscribe(_ => _inSight.Value = false);
+		_attackRange.OnTriggerExitAsObservable()
+			.Where(x => x.gameObject.name == _opponentAttackPartsName)
+			.Subscribe(_ => _inSight.Value = false);
+		_damagedParts.OnTriggerEnterAsObservable()
+			.Where(x => x.gameObject.name == _opponentAttackPartsName)
+			.Where(_ => _opponentAnimator.GetCurrentAnimatorStateInfo(0).IsName("Attack02_SwordAndShiled") == true)
+			.Subscribe(_ => _getHit.Value = true);
+		_damagedParts.OnTriggerExitAsObservable()
+			.Where(x => x.gameObject.name == _opponentAttackPartsName)
+			.Subscribe(_ => _getHit.Value = false);
+	}
+
+	/// <summary>
+	/// 当たったオブジェクトが視界内かどうか
+	/// </summary>
+	/// <param name="collider">当たったオブジェクトのcollider</param>
+	/// <param name="sightAngle">視界角度</param>
+	/// <returns></returns>
+	private bool InSightCheck(Collider collider, float sightAngle)
+	{
+		Vector3 posDelta = collider.transform.position - this.transform.position;
+		float targetAngle = Vector3.Angle(this.transform.forward, posDelta);
+		if (targetAngle <= sightAngle)
+		{
+			return true;
+		}
+		return false;
+	}
+}
