e12102aff9cfbf6638c0db9e1de06559190cc8f8
soccer4kzm8 <soccer4.kzm8@gmail.com>
2023-01-21 16:53:44 +0900

    タートルの追加

--- Assets/Scripts/Enemies/TurtleShell/TurtleShellFactory.cs
+++ Assets/Scripts/Enemies/TurtleShell/TurtleShellFactory.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class SlimeFactory : EnemyFactory
+public class TurtleShellFactory : EnemyFactory
 {
-    [SerializeField] private EnemyCore slimePrefab;
+    [SerializeField] private EnemyCore turtleShellPrefab;
     public override IEnemyCore GetEnemy(Vector3 position)
     {
-        GameObject instance = Instantiate(slimePrefab.gameObject, position, Quaternion.identity);
+        GameObject instance = Instantiate(turtleShellPrefab.gameObject, position, Quaternion.identity);
         EnemyCore slime = instance.GetComponent<EnemyCore>();
         slime.Initialize();
         return slime;
